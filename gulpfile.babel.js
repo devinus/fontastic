3a49cf6378dd207d8d3cc03d7e754d674c4f284b
Devin Torres <devin@devintorr.es>
2015-07-27 02:37:45 -0500

    gulpfile.js -> gulpfile.babel.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,16 @@
-var gulp = require('gulp');
-var sourcemaps = require('gulp-sourcemaps');
-var babel = require('gulp-babel');
-var concat = require('gulp-concat');
-var eslint = require('gulp-eslint');
+import gulp from 'gulp';
+import sourcemaps from 'gulp-sourcemaps';
+import babel from 'gulp-babel';
+import concat from 'gulp-concat';
+import eslint from 'gulp-eslint';
+
+import { name as packageName } from './package.json';
 
 gulp.task('default', ['lint'], function() {
   return gulp.src('src/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(babel({ loose: 'all' }))
-    .pipe(concat('fontastic.js'))
+    .pipe(concat(`${packageName}.js`))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist'));
 });
